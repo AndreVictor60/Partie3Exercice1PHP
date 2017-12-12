fab2d05c6a057da5683281dc3edd84b6f7e95bbe
AndreVictor60 <andre.victor60@free.fr>
2017-12-12 21:03:59 +0100

    fini

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Partie 3 Exercice 1</title>
+  </head>
+  <body>
+    <?php
+    $number=0;
+    while($number<10){
+      $number++;
+      echo $number . '</br>';
+    }
+     ?>
+
+  </body>
+</html>
